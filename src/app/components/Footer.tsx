d190d15095ed985368f3e00929f3ecf8d4987d2e
Simone <150595274+simonetimi@users.noreply.github.com>
2024-03-14 18:26:12 +0100

    feat: add footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,18 @@
-'use client';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <footer className="flex h-14 w-screen items-center border-b-1 border-gray-200 dark:border-white/20"></footer>
+    <footer className="flex h-14 w-screen items-center justify-center border-t-1 border-gray-200 dark:border-white/20">
+      <Link href="https://github.com/simonetimi" target="_blank">
+        <Image
+          alt="GitHub logo"
+          src="github-mark.svg"
+          className="transition-transform hover:scale-105 dark:invert"
+          width={24}
+          height={24}
+        />
+      </Link>
+    </footer>
   );
 }
