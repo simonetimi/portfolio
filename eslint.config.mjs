1b4a0bb24f426e478623449570c56acb24e2761c
Simone <150595274+simonetimi@users.noreply.github.com>
2025-01-11 15:30:55 +0100

    chore: fix eslint 9 flat config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,26 @@
 import typescriptEslint from '@typescript-eslint/eslint-plugin';
 import jsxA11Y from 'eslint-plugin-jsx-a11y';
-import pluginNext from '@next/eslint-plugin-next';
+import nextPlugin from '@next/eslint-plugin-next';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import js from '@eslint/js';
 import { FlatCompat } from '@eslint/eslintrc';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: import.meta.dirname,
   recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
 });
 
 export default [
+  ...compat.config({
+    extends: [
+      'next/core-web-vitals',
+      'next/typescript',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:jsx-a11y/recommended',
+      'plugin:prettier/recommended',
+      'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    ],
+  }),
   {
     ignores: [
       'next.config.mjs',
@@ -25,41 +30,11 @@ export default [
       'postcss.config.js',
       'tailwind.config.ts',
     ],
-  },
-  ...compat.extends(
-    'plugin:@typescript-eslint/recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:prettier/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'next/core-web-vitals',
-  ),
-  {
-    plugins: {
-      '@typescript-eslint': typescriptEslint,
-      'jsx-a11y': jsxA11Y,
-      'simple-import-sort': simpleImportSort,
-      '@next/next': pluginNext,
-    },
-
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-
-      parserOptions: {
-        project: './tsconfig.json',
-      },
-    },
-
     rules: {
       '@typescript-eslint/no-unused-vars': 'warn',
       'no-console': 'warn',
       'simple-import-sort/imports': 'error',
       'simple-import-sort/exports': 'error',
-    },
-  },
-  {
-    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
-    rules: {
       'simple-import-sort/imports': [
         'error',
         {
@@ -73,6 +48,26 @@ export default [
           ],
         },
       ],
+    },
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+      'jsx-a11y': jsxA11Y,
+      'simple-import-sort': simpleImportSort,
+      next: nextPlugin,
+    },
+    settings: {
+      next: {
+        rootDir: '.',
+      },
+    },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parser: typescriptEslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
     },
   },
 ];
